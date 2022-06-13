e9ff551fc777a89f25965609b06d0d9dee26eff9
Ajuka David <ajuka.zephiniah@gmail.com>
2022-06-13 16:27:28 +0100

    added variables for tracking countdown timer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,27 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
 import Timer from '@/components/Timer.vue'
+import { ref } from 'vue'
+
+const newYears = "1 Jan 2023"
+const remainingDays = ref("")
+const remainingHours = ref("")
+const remainingMinutes = ref("")
+const remainingSeconds = ref("")
+
+
+function countDown() {
+const newYearsDate: Date = new Date(newYears)
+const currentDate: Date = new Date()
+
+const totalSeconds = (newYearsDate.getTime() -  currentDate.getTime()) / 1000
+
+const days = Math.floor(totalSeconds / 3600 / 24)
+const hours = Math.floor(totalSeconds / 3600 ) % 24
+const mins = Math.floor(totalSeconds / 60) % 60
+const seconds = Math.floor(totalSeconds) % 60
+
+}
 
 </script>
 
